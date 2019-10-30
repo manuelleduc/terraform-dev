c682c31334b037f36d7016284620cac8ea75de2b
Manuel Leduc <manuel.leduc@gmail.com>
2019-10-30 23:42:12 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 variable "rancher2_access_key" {}
 variable "rancher2_secret_key" {}
+variable "rancher2_url" {}
+
 provider "rancher2" {
-  api_url = "https://51.15.206.221/v3"
+  api_url = "${var.rancher2_url}"
   access_key = "${var.rancher2_access_key}"
   secret_key = "${var.rancher2_secret_key}"
   insecure = true
