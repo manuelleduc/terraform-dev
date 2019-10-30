b147e1427255e71ee3db559dba6b742b385b242a
Manuel Leduc <manuel.leduc@gmail.com>
2019-10-30 23:39:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "rancher2" {
   api_url = "https://51.15.206.221/v3"
   access_key = "${var.rancher2_access_key}"
   secret_key = "${var.rancher2_secret_key}"
+  insecure = true
 }
 
 data "rancher2_cluster" "quickstart" {
